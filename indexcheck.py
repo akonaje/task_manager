bb98cd7dff2a83adf508c86a75babcecac37c446
akonaje <101454595+akonaje@users.noreply.github.com>
2024-12-11 23:57:33 -0500

    Update indexcheck.py

--- indexcheck.py
+++ indexcheck.py
@@ -3,6 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'http://127.0.0.1:5000'  # Replace with your actual database URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'http://127.0.0.1:5000'  
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
